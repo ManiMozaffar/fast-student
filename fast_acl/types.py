0edafcf933b457948b4c985b5ec7cbf4d084f5ca
ManiMozaffar <mani.mozaffar@gmail.com>
2023-08-21 21:53:24 +0300

    ✨ Add type hintings for types

---
+++ fast_acl/types.py
@@ -0,0 +1,7 @@
+from typing import NewType
+from uuid import UUID
+
+UserId = NewType("UserId", UUID)
+StudentId = NewType("StudentId", UUID)
+ClassRoomId = NewType("ClassRoomId", UUID)
+TeacherId = NewType("TeacherId", UUID)
