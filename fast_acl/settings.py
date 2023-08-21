346e6751f8f8a7176e87edbf70d269afd30e3349
ManiMozaffar <mani.mozaffar@gmail.com>
2023-08-22 01:13:53 +0300

    :recycle: settings

--- fast_acl/settings.py
+++ fast_acl/settings.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    SECRET_KEY = "HELLO"
-    ALGORITHM = "HS256"
+    SECRET_KEY: str = "HELLO"
+    ALGORITHM: str = "HS256"
 
 
 setting = Settings()
