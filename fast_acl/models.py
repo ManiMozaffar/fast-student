3f04ca6d0af800cdcf4b1fb0b384695a22328d24
ManiMozaffar <mani.mozaffar@gmail.com>
2023-08-22 01:15:51 +0300

    :recycle: Refactor mdoels w.r.t db design diagram

--- fast_acl/models.py
+++ fast_acl/models.py
@@ -1,44 +1,59 @@
 # Think of this file as SQL model database
 
+import uuid
 from datetime import datetime
 
 from pydantic import BaseModel, Field
 
 from fast_acl.acl.role import UserRoles
-from fast_acl.types import ClassRoomId, StudentId, TeacherId, UserId
+from fast_acl.types import (
+    AdvisorId,
+    ClassRelationId,
+    ClassRoomId,
+    StudentId,
+    TeacherId,
+    UserId,
+)
 
 
 class User(BaseModel):
-    id: UserId
+    id: UserId = Field(default_factory=uuid.uuid4, frozen=True)
     role: UserRoles
     username: str
     password: str
 
 
 class Teacher(BaseModel):
-    user_id: UserId
-    classroom_id: ClassRoomId
+    id: TeacherId = Field(default_factory=uuid.uuid4, frozen=True)
+    user_id: UserId = Field(default_factory=uuid.uuid4)
+
+
+class Admin(BaseModel):
+    id: TeacherId = Field(default_factory=uuid.uuid4, frozen=True)
+    user_id: UserId = Field(default_factory=uuid.uuid4)
 
 
 class Advisor(BaseModel):
-    user_id: UserId
+    id: AdvisorId = Field(default_factory=uuid.uuid4, frozen=True)
+    user_id: UserId = Field(default_factory=uuid.uuid4)
 
 
 class Student(BaseModel):
-    id: StudentId
+    id: StudentId = Field(default_factory=uuid.uuid4, frozen=True)
     user_id: UserId
-    classroom_id: ClassRoomId
-    grade: int
+    grade: int | None = None
+    is_expeled: bool = False
 
 
 class ClassRoom(BaseModel):
-    id: ClassRoomId
-    teacher_id: TeacherId
+    id: ClassRoomId = Field(default_factory=uuid.uuid4, frozen=True)
+    topic: str
 
 
 class ClassRoomRelation(BaseModel):
-    classroom_id: ClassRoomId
-    student_id: StudentId
-    teacher_id: TeacherId
+    id: ClassRelationId = Field(default_factory=uuid.uuid4, frozen=True)
+    classroom_id: ClassRoomId = Field(default_factory=uuid.uuid4)
+    student_id: StudentId | None = None
+    teacher_id: TeacherId | None = None
     expires_at: datetime
     created_at: datetime = Field(default_factory=datetime.utcnow)
