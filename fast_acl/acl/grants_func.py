e284e03bd91b72b63def452f33a3909ef9834411
Reza Rohani <theonlykingpin@gmail.com>
2023-08-25 13:28:20 +0330

    🎨 Improve logic expression using walrus operator (#1)
    * Update grants_func.py
    
    The walrus operator is used to assign the results of comparison operations to variables directly in the if statement, removing the need for separate assignment lines. This can make the code more compact and potentially slightly more efficient.

--- fast_acl/acl/grants_func.py
+++ fast_acl/acl/grants_func.py
@@ -24,17 +24,14 @@ async def related_classroom(
     repo = ClassroomRepository(database)
     relations = repo.filter_classroom_relations(classroom_id)
     for relation in relations:
-        is_classroom_teacher = relation.teacher_id == asked_id
-        if is_classroom_teacher:
-            result = repo.is_student_in_classroom(
-                classroom_id, student_id=StudentId(ask_for_id)
-            )
-            if not result:
+        if (is_classroom_teacher := relation.teacher_id == asked_id):
+            if not repo.is_student_in_classroom(
+                    classroom_id, student_id=StudentId(ask_for_id)
+            ):
                 raise NotAuthorizedError
             return True
 
-        is_self_student = relation.student_id == asked_id and asked_id == ask_for_id
-        if is_self_student:
+        if (is_self_student := relation.student_id == asked_id and asked_id == ask_for_id):
             return True
 
     raise NotAuthorizedError
